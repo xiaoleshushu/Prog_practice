984422c7293851b992c953243fb30e6393a3daaa
xiaoleshushu <15558169258@163.com>
2021-01-04 01:46:55 +0000

    Add binary tree path

---
+++ binary_tree/main.c
@@ -0,0 +1,16 @@
+/******************
+ *
+ *  main 文件
+ *
+ *****************/
+#include "public.h"
+
+/* external function */
+int main(int argc, char *argv[])
+{
+	//int i;
+	int rc = -1;
+	
+	return rc;
+
+}
